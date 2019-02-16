a581c131b2aeee281cdc8e0fdecc6270b5ee1e7b
Cameron Clark <cpc1007@rit.edu>
2019-02-16 15:03:00 -0500

    got posters and plots

--- omdb.py
+++ omdb.py
@@ -1,12 +1,16 @@
 """
 All the data
 """
+import json
 import requests
+import shutil
+
 
 KEY_FILE = "api.key"
 KEY = open(KEY_FILE).read()
 DATA_API = "http://www.omdbapi.com/?apikey={}&".format(KEY)
 POSTER_API = "http://img.omdbapi.com/?apikey={}&".format(KEY)
+ID_FILE = "movie_ids.json"
 
 
 def get_plot(imdbID):
@@ -20,8 +24,7 @@ def get_plot(imdbID):
         plot (string): full plot for the movie
     """
     response = requests.get("{}i={}&plot=full".format(DATA_API, imdbID))
-    plot = response.json()['plot']
-    return plot
+    return response.json()['Plot'] if response.status_code == 200 else None 
 
 def get_poster(imdbID):
     """
@@ -33,11 +36,19 @@ def get_poster(imdbID):
     Returns:
         poster (image): image file of the poster
     """
-    response = requests.get("{}i={}".format(DATA_API, imdbID))
+    response = requests.get("{}i={}".format(POSTER_API, imdbID), stream=True)
+    """
+    if response.status_code == 200:
+        with open("{}.jpg".format(imdbID), 'wb') as f:
+            response.raw.decode_content = True
+            shutil.copyfileobj(response.raw, f)
+    """
 
+    return response.raw if response.status_code == 200 else None
 
 if __name__ == '__main__':
-    test_movies = ["Hitch", "Up", "Harry Potter"]
-    for movie in test_movies:
-        get_plot(movie)
-        get_posters(movie)
+    ids = json.load(open(ID_FILE))
+    for i in ids:
+        i = "tt{}".format(i)
+        plot = get_plot(i)
+        poster = get_poster(i)
